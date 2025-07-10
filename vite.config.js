f04e55f762565f417b51bdbec9e884e907e65297
Виктор Брылёв <msne1488@gmail.com>
2025-07-11 00:03:20 +0300

    vacancies start

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint({
+      failOnError: true,
+      emitWarning: true,
+      emitError: true,
+      cache: false,
+    }),
+  ],
+});
