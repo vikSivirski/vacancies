5e059c502fcca813d7676e8b1ec1a99e81861ce2
Виктор Брылёв <msne1488@gmail.com>
2025-07-17 11:04:46 +0300

    start weblayout2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import { AppShell, Flex, Stack, Title, Text } from '@mantine/core';
+import { AppShell, Flex, Stack, Title, Text, Container, TextInput, Button } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
 
 import logo from '../../assets/image2.svg';
 import Counter from '../Counter';
@@ -20,13 +21,32 @@ function App() {
         </Flex>
       </AppShell.Header>
       <AppShell.Main>
-        <Stack gap={0}>
-          <Title order={1}>Список вакансий</Title>
-          <Text fw={500} color="#0F0F1080">
-            по профессии Frontend-разработчик
-          </Text>
-        </Stack>
-        <Counter />
+        <Container size={1000}>
+          <Flex align="center" justify="space-between">
+            <Stack gap={0}>
+              <Title order={3}>Список вакансий</Title>
+              <Text fw={500} color="#0F0F1080">
+                по профессии Frontend-разработчик
+              </Text>
+            </Stack>
+            <form>
+              <Flex gap="sm">
+                <TextInput
+                  size="md"
+                  placeholder="Должность или название компании"
+                  leftSection={<IconSearch size={16} />}
+                  style={{
+                    minWidth: 403,
+                  }}
+                />
+                <Button size="md" type="submit">
+                  Найти
+                </Button>
+              </Flex>
+            </form>
+          </Flex>
+          <Counter />
+        </Container>
       </AppShell.Main>
     </AppShell>
   );
